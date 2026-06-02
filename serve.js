83cc1744dc8c492786b516df6f1d55965a734a5d
sinkpeso <kristian.pajudzki@gmail.com>
2026-06-02 20:09:28 +0800

    modified:   index.html

---
+++ serve.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.json': 'application/json',
+  '.md': 'text/plain',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon'
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(process.cwd(), req.url === '/' ? '/index.html' : req.url);
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
